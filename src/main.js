48bad8d39fb959f394edc6f13d0fb2145f42c056
salmanorak <salman.orak@gmail.com>
2019-05-21 16:30:46 +0300

    typo fixes

--- src/main.js
+++ src/main.js
@@ -1,16 +1,16 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
-export const EventBus = new Vue();
+export const EventBus = new Vue()
 
-Vue.filter('addCountLength', function (value) { return value + ' ( ' + value.length + ' )'; })
+Vue.filter('addCountLength', function (value) { return value + ' ( ' + value.length + ' )' })
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')
--- src/reverse.js
+++ src/reverse.js
@@ -1,7 +1,7 @@
 export const exMixin = {
   computed: {
     reversedTextinMixin () {
-      return this.text4.split('').reverse().join('');
+      return this.text4.split('').reverse().join('')
     },
     addCountTextinMixin () {
       return this.text4 + ' ( ' + this.text4.length + ' )'
